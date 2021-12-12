c0cf97b2f6ad7cbd42862800caf02f8bf6fa6eb3
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-12-12 16:54:04 -0500

    add notes fix typos

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// Dependencies
-// =============================================================
+// Set up needed dependencies to run application
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -16,16 +15,19 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-// =============================================================
 
+// GET request for HTML page- index/ app home page
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/public/assets", "index.html"));
 });
 
+// GET request for HTML page - notes
 app.get("/notes", function (req, res) {
+  //sends a message to the client
   res.sendFile(path.join(__dirname + "/public/assets", "notes.html"));
 });
 
+//GET request for the notes data
 app.get("/api/notes", function (req, res) {
   fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
@@ -33,10 +35,12 @@ app.get("/api/notes", function (req, res) {
   });
 });
 
+// post notes to notes route
 app.post("/api/notes", function (req, res) {
   fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
+    // sets random number for the ID of the note per requirements
     req.body.id = uuidv4();
     notes.push(req.body);
     fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
@@ -46,9 +50,10 @@ app.post("/api/notes", function (req, res) {
   });
 });
 
+// delete notes with id that equals the random ID used
 app.delete("/api/notes/:id", function (req, res) {
   var deleteId = req.params.id;
-  console.log(deleteId);
+  //  console.log(deleteId);
   fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
@@ -63,7 +68,7 @@ app.delete("/api/notes/:id", function (req, res) {
 });
 
 // Starts the server to begin listening
-// =============================================================
+
 app.listen(PORT, function () {
   console.log("App listening on http://localhost:" + PORT);
 });
